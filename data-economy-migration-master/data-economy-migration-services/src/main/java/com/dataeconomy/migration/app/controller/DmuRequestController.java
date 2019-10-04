93bddbd2e110928abe936079d77e6071df61fe87
chidha2005 <chidha2005@gmail.com>
2019-10-04 12:48:12 +0530

    new app commit

---
+++ data-economy-migration-master/data-economy-migration-services/src/main/java/com/dataeconomy/migration/app/controller/DmuRequestController.java
@@ -0,0 +1,39 @@
+package com.dataeconomy.migration.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dataeconomy.migration.app.model.DmuBasketDTO;
+import com.dataeconomy.migration.app.model.DmuHistoryDTO;
+import com.dataeconomy.migration.app.service.DmuRequestService;
+
+@RestController
+@RequestMapping("/datamigration/request")
+public class DmuRequestController {
+
+	@Autowired
+	private DmuRequestService dmuRequestService;
+
+	@PostMapping("/save")
+	public boolean saveRequest(@RequestBody DmuHistoryDTO historyMainDto) {
+		return dmuRequestService.saveRequest(historyMainDto);
+	}
+
+	@GetMapping("/all")
+	public List<String> getAllDatabases() {
+		return dmuRequestService.getAllRequestDatabases();
+	}
+
+	@GetMapping("/all/{databaseName}")
+	public List<DmuBasketDTO> getAllTablesForGivenDatabase(@PathVariable(name = "databaseName") String databaseName) {
+		return dmuRequestService.getAllTablesForGivenDatabase(databaseName);
+	}
+
+}
